8f5d4ee927b5edd67e06925753d5e0b04a941d54
ameyawagh <ameya555@gmail.com>
2018-05-28 11:40:14 -0400

    initial commit

---
+++ readData.py
@@ -0,0 +1,84 @@
+#! /usr/bin/python3.5
+import os
+# import glob
+# import cv2
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+sys.path.append(os.path.join(os.getcwd(),"./Mask_RCNN/"))
+
+ROOT_DIR = os.path.abspath("./")
+
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+
+sys.path.append(os.path.join("./Mask_RCNN/", "samples/coco/"))  # To find local version
+import coco
+
+MODEL_DIR = os.path.join('./', "logs")
+
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+
+IMAGE_DIR = os.path.join(ROOT_DIR, "Mask_RCNN/images")
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+
+file_names = next(os.walk(IMAGE_DIR))[2]
+image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+# Run detection
+results = model.detect([image], verbose=1)
+
+# Visualize results
+r = results[0]
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+                            class_names, r['scores'])
+
+
+# DATASET_DIR = "./Train"
+
+# RGB_DIR = os.path.join(DATASET_DIR,'CameraRGB')
+# MASK_DIR = os.path.join(DATASET_DIR,'CameraSeg')
+
+# files = os.listdir(RGB_DIR)
+
